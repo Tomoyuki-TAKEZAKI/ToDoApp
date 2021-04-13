6adacff9a19b9d010a45a75a46ad5136ce00a6e9
Tomoyuki TAKEZAKI <t-takezaki@unext.jp>
2021-04-13 23:11:05 +0900

    minor fix

--- app/src/main/java/jp/takezaki/todo/ui/ListView.kt
+++ app/src/main/java/jp/takezaki/todo/ui/ListView.kt
@@ -2,7 +2,6 @@ package jp.takezaki.todo.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -16,9 +15,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.takezaki.todo.TodoItem
 import jp.takezaki.todo.viewmodel.ListViewModel
@@ -27,54 +24,38 @@ import jp.takezaki.todo.viewmodel.ListViewModel
 fun ToDoListView(model: ListViewModel = viewModel()) {
     val list by model.list.observeAsState()
 
-    ConstraintLayout(
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
     ) {
-        val addButton = createRef()
-        Column(
-            modifier = Modifier.verticalScroll(rememberScrollState())
-        ) {
-            list?.map {
-                ListItemView(it)
-            }
+        list?.map {
+            ListItemView(it)
         }
-        FloatingActionButton(
-            onClick = { model.addItem("") },
-            modifier = Modifier
-                .constrainAs(addButton) {
-                    bottom.linkTo(parent.bottom)
-                    end.linkTo(parent.end)
-                }
-                .padding(20.dp),
-            content = {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "",
-                )
-            }
-        )
+        AddButton()
     }
 }
 
 @Composable
-fun ListItemView(
-    item: TodoItem,
-    model: ListViewModel = viewModel(),
-) {
+private fun AddButton(model: ListViewModel = viewModel()) {
+    Button(
+        onClick = { model.addItem("") },
+        modifier = Modifier.padding(10.dp),
+        content = {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = null,
+            )
+        }
+    )
+}
+
+@Composable
+private fun ListItemView(item: TodoItem) {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
         ItemCheckboxView(item)
         ItemTextView(item)
-        Button(
-            onClick = {
-                model.removeItem(item)
-            },
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
-            modifier = Modifier.padding(5.dp),
-        ) {
-            Icon(imageVector = Icons.Default.Delete, contentDescription = "")
-        }
+        DeleteButton(item)
     }
 }
 
@@ -104,4 +85,23 @@ private fun ItemTextView(
         },
         modifier = Modifier.padding(5.dp),
     )
+}
+
+@Composable
+private fun DeleteButton(
+    item: TodoItem,
+    model: ListViewModel = viewModel(),
+) {
+    Button(
+        onClick = {
+            model.removeItem(item)
+        },
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+        modifier = Modifier.padding(5.dp),
+    ) {
+        Icon(
+            imageVector = Icons.Default.Delete,
+            contentDescription = null
+        )
+    }
 }
