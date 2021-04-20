562a46b01a18d56a1b5eebeaea26349d2baa2f17
Tomoyuki TAKEZAKI <t-takezaki@unext.jp>
2021-04-20 14:33:35 +0900

    review

--- app/src/main/java/jp/takezaki/todo/TodoItem.kt
+++ app/src/main/java/jp/takezaki/todo/TodoItem.kt
@@ -2,7 +2,8 @@ package jp.takezaki.todo
 
 import java.time.LocalDateTime
 
-class TodoItem private constructor(
+data class TodoItem private constructor(
+    val id: Int,
     val name: String,
     val isDone: Boolean,
     val creationDateTime: LocalDateTime,
@@ -12,20 +13,64 @@ class TodoItem private constructor(
 
     companion object Factory {
         fun getNewItem(name: String): TodoItem =
-            TodoItem(name, false, LocalDateTime.now(), "", null)
+            TodoItem(
+                // DB（永続層）との連携
+                // idの一意性（データの生成）について誰が責任を持つのか？
+                // c.f. CRUD
+                LocalDateTime.now().hashCode(), // 衝突しない保証は？
+                name,
+                false,
+                LocalDateTime.now(),
+                "",
+                null
+            )
 
+        // data class なら以下を copy で表現できる
+        // fun Item.updateName など
         fun getItemWithUpdatedName(item: TodoItem, newName: String): TodoItem =
-            TodoItem(newName, item.isDone, item.creationDateTime, item.detailText, item.dueDateTime)
+            TodoItem(
+                item.id,
+                newName,
+                item.isDone,
+                item.creationDateTime,
+                item.detailText,
+                item.dueDateTime
+            )
 
         fun getItemWithUpdatedCheckBox(item: TodoItem, isDone: Boolean): TodoItem =
-            TodoItem(item.name, isDone, item.creationDateTime, item.detailText, item.dueDateTime)
+            TodoItem(
+                item.id,
+                item.name,
+                isDone,
+                item.creationDateTime,
+                item.detailText,
+                item.dueDateTime
+            )
 
         fun getItemWithUpdatedDetailText(item: TodoItem, detailText: String): TodoItem =
-            TodoItem(item.name, item.isDone, item.creationDateTime, detailText, item.dueDateTime)
+            TodoItem(
+                item.id,
+                item.name,
+                item.isDone,
+                item.creationDateTime,
+                detailText,
+                item.dueDateTime
+            )
 
         fun getItemWithUpdatedDueDate(item: TodoItem, dueDateTime: LocalDateTime?): TodoItem =
-            TodoItem(item.name, item.isDone, item.creationDateTime, item.detailText, dueDateTime)
+            TodoItem(
+                item.id,
+                item.name,
+                item.isDone,
+                item.creationDateTime,
+                item.detailText,
+                dueDateTime
+            )
     }
 
-    infix fun shouldBeUpdatedBy(other: TodoItem): Boolean = creationDateTime == other.creationDateTime
+    infix fun shouldBeUpdatedBy(other: TodoItem): Boolean = id == other.id
+
+    // 同値性を定義すれば、mapでそのアイテムだけ更新すれば良い
+    // compose内部での差分検知はどうなっている？ RecyclerViewのdiffUtilのような仕組みは？
+
 }
